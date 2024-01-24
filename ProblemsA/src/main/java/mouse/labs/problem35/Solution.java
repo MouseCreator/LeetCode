d9729c32e7d7620d1fb42ae3b6fa4efcf4f22ff6
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-01-24 03:09:45 +0200

    added solution for problem 35

---
+++ ProblemsA/src/main/java/mouse/labs/problem35/Solution.java
@@ -0,0 +1,22 @@
+package mouse.labs.problem35;
+
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length;
+        int mid;
+        while (low < high) {
+            mid = (high + low) >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return high;
+
+    }
+}
---
+++ ProblemsA/src/test/java/mouse/labs/problem35/SolutionTest.java
@@ -0,0 +1,16 @@
+package mouse.labs.problem35;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void searchInsert() {
+        Solution solution = new Solution();
+        assertEquals(2, solution.searchInsert(new int[]{1,3,5,6}, 5));
+        assertEquals(1, solution.searchInsert(new int[]{1,3,5,6}, 2));
+        assertEquals(4, solution.searchInsert(new int[]{1,3,5,6}, 7));
+    }
+}
