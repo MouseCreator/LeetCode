f42826cffdce781ca5307290ebb0f94189d804c3
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-08-26 17:14:28 +0300

    solved problem 6

--- Python solutions/6/Zigzag Conversion.py
+++ Python solutions/6/Zigzag Conversion.py
@@ -1,12 +1,38 @@
 class Solution(object):
     def convert(self, s, numRows):
-        symbolsZigZaging = numRows - 2
         lens = len(s)
-        expected_columns = (lens - numRows) // (numRows + symbolsZigZaging) + 1
-        print(expected_columns)
+        if numRows == 1 or lens <= numRows:
+            return s
+        symbolsZigZaging = numRows - 2
+        positions = []
+        step = numRows + symbolsZigZaging
+        after_step = 0
+        for i in range(numRows):
+            j = i
+            positions.append(j)
+            modified = True
+            while modified:
+                j = j + step
+                modified = False
+                if j < lens and step > 0:
+                    positions.append(j)
+                    modified = True
+                j = j + after_step
+                if j < lens and after_step > 0:
+                    positions.append(j)
+                    modified = True
+            step = step - 2
+            after_step = after_step + 2
+        res_str = ""
+        for p in positions:
+            res_str = res_str + s[p]
+        return res_str
+
+
+
 
 
 
 
 st = Solution()
-print(st.convert("PAYPALISHIRING", 4))
+print(st.convert("ABCDEFGHIJKLMNOPQRS", 6))
